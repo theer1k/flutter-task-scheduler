a77203f5d4376d51d15208c88271e1289958e534
Erik Henrique <erik.hac@outlook.com>
2019-06-09 13:17:30 -0300

    Adding tasks on list

--- lib/main.dart
+++ lib/main.dart
@@ -9,35 +9,98 @@ void main() => runApp(MaterialApp(
     ));
 
 class Home extends StatefulWidget {
-  List _toDoList = [];
+  // Future<File> _getFile() async {
+  //   final Directory directory = await getApplicationDocumentsDirectory();
+  //   return File("${directory.path}/data.json");
+  // }
 
-  Future<File> _getFile() async {
-    final Directory directory = await getApplicationDocumentsDirectory();
-    return File("${directory.path}/data.json");
-  }
+  // Future<File> _saveData() async {
+  //   String data = json.encode(this._toDoList);
+  //   final file = await _getFile();
+  //   return file.writeAsString(data);
+  // }
 
-  Future<File> _saveData() async {
-    String data = json.encode(_toDoList);
-    final file = await _getFile();
-    return file.writeAsString(data);
-  }
-
-  Future<String> _readData() async {
-    try {
-      final file = await _getFile();
-      return file.readAsString();
-    } catch(err) {
-      return null;
-    }
-  }
+  // Future<String> _readData() async {
+  //   try {
+  //     final file = await _getFile();
+  //     return file.readAsString();
+  //   } catch (err) {
+  //     return null;
+  //   }
+  // }
 
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  final _toDoController = TextEditingController();
+
+  List _toDoList = [];
+
+  void _addTodo() {
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["ok"] = false;
+      _toDoList.add(newToDo);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: _toDoController,
+                    decoration: InputDecoration(
+                        labelText: "Nova Tarefa",
+                        labelStyle: TextStyle(color: Colors.blueAccent)),
+                  ),
+                ),
+                RaisedButton(
+                  color: Colors.blueAccent,
+                  child: Text("Adicionar"),
+                  textColor: Colors.white,
+                  onPressed: _addTodo,
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                padding: EdgeInsets.only(top: 10.0),
+                itemCount: _toDoList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return new CheckboxListTile(
+                    title: Text(_toDoList[index]["title"]),
+                    value: _toDoList[index]["ok"],
+                    secondary: CircleAvatar(
+                      child: Icon(
+                          _toDoList[index]["ok"] ? Icons.check : Icons.error),
+                    ),
+                    onChanged: (checked) {
+                      setState(() {
+                        _toDoList[index]["ok"] = checked;
+                      });
+                    },
+                  );
+                }),
+          )
+        ],
+      ),
+    );
   }
 }
