e73e5aab33c41e0aeb4bdc969c1c5b906158cf2f
Erik Henrique <erik.hac@outlook.com>
2019-06-14 08:11:54 -0300

    Undo delete snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,8 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final _toDoController = TextEditingController();
-
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
   @override
   void initState() {
     super.initState();
@@ -124,6 +125,31 @@ class _HomeState extends State<Home> {
           });
         },
       ),
+      onDismissed: (DismissDirection direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+                setState(() {
+                  _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                  _saveData();
+                });
+              },
+            ),
+            duration: Duration(seconds: 3),
+          );
+
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 }
